7229ce85baba8efd286a3989009d8fd2be75fc26
maboloshi <ymc1216@gmail.com>
2020-07-13 13:26:19 +0800

    修复 bug 控制台下有提示错误 1. 忽略.git文件夹 2. 修复不同平台选用特定主菜单的问题 3. 留下一个问题 只当需翻译插件存在的情况下, 对其插件进行翻译 (可能需要引入 EventListener)    否则会产生"无效"菜单

--- main.py
+++ main.py
@@ -7,19 +7,25 @@ def plugin_loaded():
 	os.chdir(os.path.abspath(os.path.dirname(__file__)))
 	packages_path=sublime.packages_path()
 	for item in os.listdir():
-		if os.path.isdir(item):
+		if os.path.isdir(item) and item != ".git":
 			#original_dir=item
 			#target_dir="../"+ item
 			original_dir=os.path.abspath(item)
 			target_dir=os.path.join(packages_path, item)
 			if not os.path.isdir(target_dir): os.mkdir(target_dir)
-			for file in os.listdir(item):
-				#if file.endswith('.json'):
+			for file in sorted(os.listdir(item), reverse=True):
 				target_file=file.replace('.json','')
+
+				# OSX 平台 使用无快捷键提示版本
+				# 之前使用sorted降序处理, 规避了Main (OSX).sublime-menu被
+				# Main.sublime-menu复写的问题
+				if file =='Main (OSX).sublime-menu.json':
+					if sublime.platform() == 'osx':
+						target_file='Main.sublime-menu'
+					else:
+						continue
 				shutil.copy(os.path.join(original_dir, file),os.path.join(target_dir, target_file))
-			# OSX 平台 使用无快捷键提示版本
-			osx_menu_file="Main (OSX).sublime-menu"
-			target_file="Main.sublime-menu"
-			if (sublime.platform() == 'osx' and
-				os.path.isfile(os.path.join(target_dir, osx_menu_file))):
-				shutil.move(os.path.join(target_dir, osx_menu_file), os.path.join(target_dir, target_file))
+
+# 下一步:
+# 读取`Package Control.sublime-settings`下的`installed_packages`
+# 读取`Preferences.sublime-settings`下的`ignored_packages`
